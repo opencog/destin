f5696b9b7f56c7101f1cbe381cbb77c0d64d79f3
Ted Sanders <tpsjr7@gmail.com>
2012-10-24 14:25:50 -0500

    rename  create_ctags.sh

--- Destin/create_ctags.sh
+++ Destin/create_ctags.sh
@@ -1,2 +1,4 @@
+#creates ctags to be used by vim.
 #ignores files that start with an underscore
 find . ! -path "*/CMakeFiles*" -a  -regex ".*\.\(c\|cpp\|h\)" -a ! -name "_*" | xargs ctags --defines 
+
