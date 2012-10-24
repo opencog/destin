8bff57c429bee3173941894e640eea08b9c71951
Ted Sanders <tpsjr7@gmail.com>
2012-10-24 10:40:47 -0500

    WebcamExperiment: show beliefs of 1st node of each layer

--- Destin/Misc/WebcamExperiment.cpp
+++ Destin/Misc/WebcamExperiment.cpp
@@ -43,13 +43,12 @@ int main(int argc, char ** argv){
     av_log_set_level(AV_LOG_QUIET);//turn off message " No accelerated colorspace conversion found from yuv422p to bgr24"
 
     //VideoSource vs(false, "./destin_video_test.avi");
-    VideoSource vs(false, "./cowboy.avi");
-    //vs.enableDisplayWindow();
+    VideoSource vs(true, "./cowboy.avi");
+    vs.enableDisplayWindow();
 
     SupportedImageWidths siw = W512;
 
-    //uint centroid_counts[]  = {3,3,3,3,3,3,3,2};
-    uint centroid_counts[]  = {3,3,3,3,3,3,3,2};
+    uint centroid_counts[]  = {9,8,7,7,5,4,3,2};
 
     DestinNetworkAlt * network = new DestinNetworkAlt(siw, 8, centroid_counts);
 
@@ -70,33 +69,28 @@ int main(int argc, char ** argv){
 
         network->doDestin(t.getDest());
 
-        if(frameCount % 5 != 0 ){ //only print every 3rd so display is not so jumpy
+        if(frameCount % 1 != 0 ){ //only print every 3rd so display is not so jumpy
             printFPS(false);
             continue;
         }
 
-
+        
         printf("\033[2J");      /* clear screen */
         printf("Frame: %i\n", frameCount);
         printFPS(true);
-
-        Node & n = *network->getNode(0,0,0);
-        printf("Node 0,0,0 winner: %i\n", n.winner);
+        int layer = 1;
+        Node & n = *network->getNode(layer,0,0);
+        printf("Node %i,0,0 winner: %i\n",layer, n.winner);
         printf("Node centroids: %i\n", n.nb);
 
         printf("Node strav:");
         printFloatArray(n.starv, n.nb);
         printf("Starv coef: %f \n", n.starvCoeff);
         printf("\n");
-//
-//        printf("top node:\n");
-//        network->printBeliefGraph(7,0,0);
-//        printf("bottom node:\n");
-//        network->printBeliefGraph(0,0,0);
 
-        printf("layer 0 node 0 centroid locations:\n");
-        network->printNodeCentroidPositions(0,0,0);
-        for(int l = 0 ; l < 7 ; l++){
+        printf("layer %i node 0 centroid locations:\n", layer);
+        network->printNodeCentroidPositions(layer, 0, 0);
+        for(int l = 0 ; l < 8 ; l++){
             printf("belief graph layer: %i\n",l);
             network->printBeliefGraph(l,0,0);
         }
