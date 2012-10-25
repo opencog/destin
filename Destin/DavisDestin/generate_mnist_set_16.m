76cbc959fd6d51f2a325c5aecf3fdb6989db15e0
Andrew <andrew@localhost.localdomain>
2012-10-25 14:35:20 -0400

    Updated data generation matlab files

--- Destin/DavisDestin/generate_mnist_set_16.m
+++ Destin/DavisDestin/generate_mnist_set_16.m
@@ -1,7 +1,6 @@
 nR = 16;
 nMnist = 28;
 
-nSamples = 60000;
 
 points = [0  0
           0  2
@@ -26,23 +25,37 @@ points = [0  0
 
 points = points + 1;
 
-mnistFiles = {'mnist_data/train-images-idx3-ubyte', 'mnist_data/train-images-idx3-ubyte', 'mnist_data/test-images-idx3-ubyte'};
-outFiles = {'data/destin_train_16.bin', 'data/destin_train_nn_16.bin', 'data/destin_test_nn_16.bin'};
-nSamples = [20000 60000 10000];
+mnistDataFiles = {'mnist_data/train-images-idx3-ubyte', 'mnist_data/train-images-idx3-ubyte', 'mnist_data/t10k-images-idx3-ubyte'};
+mnistLabelFiles = {'mnist_data/train-labels-idx1-ubyte', 'mnist_data/train-labels-idx1-ubyte', 'mnist_data/t10k-labels-idx1-ubyte'};
+outDataFiles = {'data/train.bin', 'data/destin_train_nn_16.bin', 'data/destin_test_nn_16.bin'};
+outLabelFiles = {'data/labels.bin', 'data/labels_train_nn_16.bin', 'data/labels_test_nn_16.bin'};
+nSamples = [60000 60000 10000];
 
-for f=1:3
-	mnistSet = loadMNISTImages(mnistFiles{f})';
+digitsList = 0:2;
+
+for f=1:1
+	mnistSet = loadMNISTImages(mnistDataFiles{f})';
+    mnistLabels = loadMNISTLabels(mnistLabelFiles{f});
 	
-	fid = fopen(outFiles{f}, 'w');
+	fidData = fopen(outDataFiles{f}, 'w');
+    fidLabels = fopen(outLabelFiles{f}, 'w');
 	
-	for d=1:nSamples
+	for d=1:nSamples(f)
+        if sum(mnistLabels(d) == digitsList) == 0
+            continue;
+        end
+            
 	    tmpImage = reshape(mnistSet(d,:), nMnist, nMnist);
 	    
-	    for i=1:length(points)
-	        tmpCropImage = tmpImage(points(i,1):points(i,1)+15, points(i,2):points(i,2)+15);
-	        fwrite(fid, reshape(tmpCropImage, 1, nR*nR), 'float');
-	    end
+	    %for i=1:length(points)
+        for i=1:3
+	        %tmpCropImage = tmpImage(points(i,1):points(i,1)+15, points(i,2):points(i,2)+15)';
+            tmpCropImage = imresize(tmpImage, [nR nR])';
+            tmpCropImage(tmpCropImage < 0) = 0;
+            fwrite(fidLabels, mnistLabels(d), 'uint32');
+	        fwrite(fidData, reshape(tmpCropImage, 1, nR*nR), 'float');
+        end
 	end
 	
-	fclose(fid);
+	fclose(fidData);
 end
