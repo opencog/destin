1e6a050f36de0e3a5174f808b7a0a5abe9201a56
Ted Sanders <tpsjr7@gmail.com>
2012-12-05 10:16:36 -0600

    improve python bindings

--- Destin/Bindings/Python/dostuff.py
+++ Destin/Bindings/Python/dostuff.py
@@ -9,12 +9,25 @@ def array_to_pointer(arr):
     return ia
 
 
-        
-dn = pd.DestinNetworkAlt(pd.W512, 8, [2,2,2,2,2,2,2,2])
 
-vs = pd.VideoSource(False, "IMG_1183.MOV")
+centroids = [2,3,3,3,4,4,4,2]
+layers = len(centroids)
+top_layer = layers - 1
+dn = pd.DestinNetworkAlt(pd.W512, layers, centroids)
+
+top_node = dn.getNode(top_layer, 0, 0)
+
+
+vs = pd.VideoSource(False, "hand.MOV")
 vs.enableDisplayWindow()
 
+layerMask = pd.SWIG_UInt_Array_frompointer(dn.getNetwork().layerMask)
+
+#viz = pd.GenerativeVisualizer(dn.getNetwork())
+
+def getNodeChild(parent_node, child_num):
+    return pd.SWIG_Node_p_Array_getitem(parent_node.children, child_num)
+                                          
 def doFrame():
     if(vs.grab()):
         dn.doDestin(vs.getOutput())
@@ -22,4 +35,99 @@ def doFrame():
     else:
         return False
 
+
+def doFrames(frames):
+    for i in range(frames):
+        if not doFrame():
+            return False
+    return True
+
+
+
+def dlf(layer, start, end):
+    pd.DisplayLayerFeatures(dn.getNetwork(), layer, start, end)
+
+def doFramesWithFeatures(frames, layer, start_node, end_node):
+    for i in range(frames):
+        if(vs.grab()):
+            dn.doDestin(vs.getOutput())
+            dlf(layer, start_node, end_node)
+        else:
+            return False
+    return True
+
+def doFramesWithWinningGrid(frames, layer):
+    for i in range(frames):
+        doFrame()
+        dn.printWinningCentroidGrid(layer)
+
+def doFramesWithCallback(frames, callback):
+    for i in range(frames):
+        doFrame()
+        callback()
+
+def beliefAndGridCallback():
+    dn.printBeliefGraph(top_layer, 0, 0)
+    dn.imageWinningCentroidGrid(1, 4)
+    dlf(0, 0, 1)
+    
+def freezeTraining():
+    dn.setIsPOSTraining(False)
+    #dn.setIsPSSATraining(False)
+
+def freezeLayer(layer):
+    layerMask[layer] = 0
+
+def unfreezeLayer(layer):
+    layerMask[layer] = 1
+
+def unfreezeAllLayers():
+    for i in range(layers):
+        layerMask[i] = 1
+
+
+
+def freezeTopCentroidsExcept(lucky_centroid):
+    if lucky_centroid == None:
+        return
+    starv = pd.SWIG_FloatArray_frompointer(top_node.starv)
+    small = 1e-6
+    for c in range(top_node.nb):
+        starv[c] = 1
+
+    starv[lucky_centroid] = small
+
+def teachCentroid(centroid, frames=20):
+    unfreezeLayer(top_layer)
+    lucky_centroid = centroid
+    go(frames)
+    lucky_centroid = None
+    freezeTraining()
+    
+
+def slowFreeze(start_layer, end_layer, frames_between):
+    for l in range(start_layer, end_layer + 1):
+        go(frames_between)
+        freezeLayer(l)
+
+def the_callback():
+    dn.imageWinningCentroidGrid(0, 4, "winning layer 0")
+    dn.imageWinningCentroidGrid(1, 8, "winning layer 1")
+    dn.imageWinningCentroidGrid(2, 16, "winning layer 2")
+    #dn.imageWinningCentroidGrid(3, 16, "winning layer 3")
+    #dn.imageWinningCentroidGrid(4, 16, "winning layer 4")
+    dn.imageWinningCentroidGrid(5, 16, "winning layer 5")
+    dn.printBeliefGraph(top_layer, 0, 0)
+    freezeTopCentroidsExcept(lucky_centroid)
+    
+def go(frames=200):
+    doFramesWithCallback(frames, the_callback)
+
+        
+lucky_centroid = None
+
+    
+
+#dn.load("hand.dst")
+#freezeTraining()
 
--- Destin/Bindings/swig_destin_common.i
+++ Destin/Bindings/swig_destin_common.i
@@ -15,6 +15,7 @@
 %}
 
 %include "macros.h"
+typedef unsigned int uint;
 /* 
 turn on director wrapping callback, so c++ code can call methods defined in the target language
 See http://www.swig.org/Doc2.0/SWIGDocumentation.html#Java_directors
@@ -24,11 +25,6 @@ See https://swig.svn.sourceforge.net/svnroot/swig/trunk/Examples/java/callback/
 %feature("director") DestinIterationFinishedCallback;
 %include "DestinIterationFinishedCallback.h"
 
-/* carrays.i so you can use a c++ pointer like an array */
-%include "carrays.i" 
-%array_class(int, SWIG_IntArray);
-%array_class(float, SWIG_FloatArray);
-%array_class(uint, SWIG_UInt_Array);
 
 /* lets you use java strings easily with c++ strings */
 %include "std_string.i"
@@ -39,8 +35,18 @@ See https://swig.svn.sourceforge.net/svnroot/swig/trunk/Examples/java/callback/
 
 /* the other classes to generate wrappers for */
 %include "destin.h"
+%include "node.h"
 %include "VideoSource.h"
 %include "Transporter.h"
 %include "DestinNetworkAlt.h"
 %include "GenerativeVisualizer.h"
+%include "learn_strats.h"
 
+/* carrays.i so you can use a c++ pointer like an array */
+%include "carrays.i" 
+%array_class(int, SWIG_IntArray);
+%array_class(float, SWIG_FloatArray);
+%array_functions(float *, SWIG_Float_p_Array);
+%array_class(uint, SWIG_UInt_Array);
+%array_functions(Node *, SWIG_Node_p_Array);
+%array_class(Node, SWIG_NodeArray);
