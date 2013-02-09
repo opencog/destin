69ad1745085fb6aec5d60638064bdd6932905b8f
Ted Sanders <tpsjr7@gmail.com>
2013-02-09 16:04:51 -0600

    BeliefExporter: gets a winning centroid tree to be used in subtree mining.

--- Destin/Common/BeliefExporter.h
+++ Destin/Common/BeliefExporter.h
@@ -13,21 +13,52 @@ class BeliefExporter {
     DestinNetworkAlt & destin;
     uint outputSize;
     uint skipSize;
+    short * winnerTree;
+    const int winningTreeSize;
+    uint labelBucket;
+    int bottomLayer;
+    const int nLayers;
+
+    short getTreeLabelForCentroid(int centroid, int layer){
+        return layer * labelBucket + centroid;
+    }
+
+    int buildTree(Node * parent, int pos){
+        winnerTree[pos] = getTreeLabelForCentroid(parent->winner, parent->layer);
+        if(parent->layer > bottomLayer && parent->children != NULL){
+            for(int i = 0 ; i < 4 ; i++){
+                pos = buildTree(parent->children[i], ++pos);
+                winnerTree[++pos] = -1;
+            }
+        }
+        return pos;
+    }
+
 public:
-    const unsigned int nLayers;
 
     /** Constructor
       * @param network - destin network to export belielfs
       * @param bottom - layer to start including the beliefs for. See getOutputSize()
       */
     BeliefExporter(DestinNetworkAlt & network, uint bottom):
-        destin(network),
-        nLayers(destin.getLayerCount())
-        {
+        destin(network), winnerTree(NULL), bottomLayer(bottom),
+        nLayers(destin.getLayerCount()),
+        winningTreeSize((destin.getNetwork()->nNodes - 1) * 2 + 1)
+    {
+        labelBucket = ( 1 << ( sizeof(short) * 8 - 1))/nLayers;
         setBottomLayer(bottom);
+        for(int i = 0 ; i <  nLayers; i++){
+            if( network.getBeliefsPerNode(i) >= labelBucket){
+                throw std::domain_error("BeliefExporter: too many centroids\n.");
+            }
+        }
     }
 
     ~BeliefExporter(){
+        if(winnerTree!=NULL){
+            delete [] winnerTree;
+            winnerTree = NULL;
+        }
     }
 
     void setBottomLayer(unsigned int bottom){
@@ -58,11 +89,28 @@ public:
     /** Get the pointer to the begining of the destin beliefs.
       * The begining of the belief vector depends on what
       * the bottom layer is set to via the constructor or by
-      * setBottomLayer method. The end of the vector is
+      * setBottomLayer methobottomLayerd. The end of the vector is
       * beliefs of the top layer node.
+      * Do not delete or free this pointer.
       */
     float * getBeliefs(){
         return &(destin.getNetwork()->belief[skipSize]);
+    }
+
+    short * getWinningCentroidTree(){
+        if(!destin.getNetwork()->isUniform){
+            throw std::logic_error("BeliefExpoerter::getWinningCentroidTree only uniform destin is supported.\n");
+        }
+        if(winnerTree==NULL){
+            winnerTree = new short[getWinningCentroidTreeSize()];
+        }
+
+        buildTree(destin.getNode(nLayers - 1, 0, 0), 0);
+        return winnerTree;
+    }
+
+    int getWinningCentroidTreeSize(){
+        return winningTreeSize;
     }
 
 };
