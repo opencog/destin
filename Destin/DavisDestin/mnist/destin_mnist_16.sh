35d1d335540214d7f801bf0ce1d74979f66cbef1
jswiergo <jswiergo@gmail.com>
2013-09-21 13:11:10 +0200

    Add DeSTIN config description; Update MINST experiment parameters

--- Destin/DavisDestin/mnist/destin_mnist_16.sh
+++ Destin/DavisDestin/mnist/destin_mnist_16.sh
@@ -1,5 +1,4 @@
 #bin/bash
 
-../bin/destin train config_16 data/train_out.bin data/train.bin data/labels.bin
-../bin/destin test config_16 data/train_out.bin data/train.bin data/labels.bin
-
+#../bin/destin train config_16 data/train_out.bin data/train.bin data/labels.bin 0
+../bin/destin test config_16 data/train_out.bin data/train.bin data/labels.bin 1
--- Destin/DavisDestin/mnist/destin_mnist_32.sh
+++ Destin/DavisDestin/mnist/destin_mnist_32.sh
@@ -1,3 +1,7 @@
 #bin/bash
 
-../bin/destin config_32 data/destin_train_32.bin data/destin_train_nn_32.bin data/destin_train_out_32.bin data/destin_test_nn_32.bin data/destin_test_out_32.bin
+../bin/destin train config_32 data/destin_train_32_out.bin data/destin_train_32.bin data/labels.bin 0
+../bin/destin test config_32 data/destin_train_32_out.bin data/destin_train_32.bin data/labels.bin 0
+
+../bin/destin train config_32 data/destin_train_nn_32_out.bin data/destin_train_nn_32.bin data/labels.bin 0
+../bin/destin test config_32 data/destin_train_nn_32_out.bin data/destin_train_nn_32.bin data/labels.bin 0
